deba9fc5c0ae0f9973eec8fbba81a53b997f70c5
Matthew Southcott <mjsouthcott@gmail.com>
2020-03-15 12:28:28 -0400

    Add dynamic date and time display, and text area formatting

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 let $content = $('#content')
-let $currentDay = $('#current-day')
+let $currentDateTime = $('#current-date-time')
 
 // Define and initialize variables
-let hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-let eventsText
+let hours = [
+    { displayFormat: '9AM', twentyFourHourFormat: 9 },
+    { displayFormat: '10AM', twentyFourHourFormat: 10 },
+    { displayFormat: '11AM', twentyFourHourFormat: 11 },
+    { displayFormat: '12PM', twentyFourHourFormat: 12 },
+    { displayFormat: '1PM', twentyFourHourFormat: 13 },
+    { displayFormat: '2PM', twentyFourHourFormat: 14 },
+    { displayFormat: '3PM', twentyFourHourFormat: 15 },
+    { displayFormat: '4PM', twentyFourHourFormat: 16},
+    { displayFormat: '5PM', twentyFourHourFormat: 17 }
+]
+let eventsText, eventState
 
 // Check if `eventsText` exists in localStorage. If no, initialize and set it; if yes, get it
 if (!window.localStorage.getItem('eventsText')) {
@@ -13,24 +23,53 @@ if (!window.localStorage.getItem('eventsText')) {
     eventsText = JSON.parse(window.localStorage.getItem('eventsText'))
 }
 
-// Create content using for loop
-for (let i = 0; i < hours.length; i++) {
-    // Create `row` and `col` divs, and initialize their content
-    let $row = $(`<div class="row" id="${i}"></div>`)
-    let $hourCol = $(`<div class="col hour"><p>${hours[i]}</p></div>`)
-    let $eventCol = $(`<div class="col event past"><textarea class="form-control">${eventsText[i]}</textarea></div>`)
-    let $saveCol = $(`<div class="col save"><button class="save-button"><i class="far fa-save"></button></div>`) 
-    
-    // Append `col` divs to `row` div
-    $row.append($hourCol)
-    $row.append($eventCol)
-    $row.append($saveCol)
+// Display initial current date and time
+$currentDateTime.text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'))
 
-    // Append `row` div to `content` div
-    $content.append($row)
+// Call `addContent` function to display initial content
+addContent(moment().format('h'))
+
+// Call `setInterval` function to update current date and time, and content each second
+setInterval(function() {
+    $currentDateTime.text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'))
+
+    // Call `addContent` function at the top of every hour to update display
+    if (moment().format('mm') === '00') {
+        $content.empty()
+        addContent(moment().format('h'))
+    }
+}, 1000)
+
+// Define `addContent` function
+function addContent(currentHour) {
+    // Create content using for loop
+    for (let i = 0; i < hours.length; i++) {
+        // Set `eventState` by comparing current hour to hour represented by `row` div
+        if (Number(currentHour) > hours[i].twentyFourHourFormat) {
+            eventState = 'past'
+        } else if (Number(currentHour) === hours[i].twentyFourHourFormat) {
+            eventState = 'present'
+        } else {
+            eventState = 'future'
+        }
+
+        // Create `row` and `col` divs, and initialize their content
+        let $row = $(`<div class="row" id="${i}"></div>`)
+        let $hourCol = $(`<div class="col hour"><p>${hours[i].displayFormat}</p></div>`)
+        let $eventCol = $(`<div class="col event ${eventState}"><textarea class="form-control">${eventsText[i]}</textarea></div>`)
+        let $saveCol = $(`<div class="col save"><button class="save-button"><i class="far fa-save"></button></div>`) 
+    
+        // Append `col` divs to `row` div
+        $row.append($hourCol)
+        $row.append($eventCol)
+        $row.append($saveCol)
+
+        // Append `row` div to `content` div
+        $content.append($row)
+    }
 }
 
-// Set event listener to `content` div to respond to `click` events on `save-button` buttons
+// Add event listener to `content` div to respond to `click` events on `save-button` buttons
 $content.on('click', '.save-button', function() {
     $this = $(this)
 
